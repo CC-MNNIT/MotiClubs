b45befdf2bcaf4c3269b7a77c495d0c2343c4424
yashakabra <yashakabra05@gmail.com>
2023-01-02 22:04:34 +0530

    Added Fragements

--- app/src/main/java/com/example/notificationapp/view/activities/MainActivity.java
+++ app/src/main/java/com/example/notificationapp/view/activities/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.notificationapp.view.activities;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +11,11 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.notificationapp.R;
+import com.example.notificationapp.view.fragments.AboutUsFragment;
+import com.example.notificationapp.view.fragments.AdminPanelFragment;
+import com.example.notificationapp.view.fragments.HelpFragment;
+import com.example.notificationapp.view.fragments.HomeFragment;
+import com.example.notificationapp.view.fragments.NotificationsFragemnt;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setReferences();
         setListeners();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
 
     private void setListeners() {
@@ -32,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout.addDrawerListener(drawerToggle);
         drawerToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView.bringToFront();
+        navigationView.setCheckedItem(R.id.home);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -39,11 +47,32 @@ public class MainActivity extends AppCompatActivity {
                     //TODO
                     case R.id.home:
                     {
-                        Toast.makeText(MainActivity.this,"Home",Toast.LENGTH_SHORT).show();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+                        break;
                     }
-
+                    case R.id.notifications:
+                    {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new NotificationsFragemnt()).commit();
+                        break;
+                    }
+                    case R.id.admin_panel:
+                    {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AdminPanelFragment()).commit();
+                        break;
+                    }
+                    case R.id.about_us:
+                    {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AboutUsFragment()).commit();
+                        break;
+                    }
+                    case R.id.help:
+                    {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HelpFragment()).commit();
+                        break;
+                    }
                 }
-                return false;
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
             }
         });
     }
