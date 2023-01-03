dff7d049eadaf270ea30f3f977307d0ba3695d7a
hackeramitkumar <amit9116260192@gmail.com>
2023-01-03 15:16:43 +0530

    added already login check

--- app/src/main/java/com/example/notificationapp/view/activities/SplashScreenActivity.java
+++ app/src/main/java/com/example/notificationapp/view/activities/SplashScreenActivity.java
@@ -10,9 +10,10 @@ import android.widget.TextView;
 
 import com.example.notificationapp.R;
 import com.google.android.material.imageview.ShapeableImageView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SplashScreenActivity extends AppCompatActivity {
-
+private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,15 +21,20 @@ public class SplashScreenActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         TextView text = (TextView)findViewById(R.id.text);
         ShapeableImageView image = (ShapeableImageView)findViewById(androidx.appcompat.R.id.image);
-
+        mAuth = FirebaseAuth.getInstance();
         image.animate().translationY(-1600F).setDuration(1000).setStartDelay(3000);
         text.animate().translationY(1400F).setDuration(1000).setStartDelay(3000);
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(getApplicationContext(), IntroSliderActivity.class);
-                startActivity(intent);
+                if(mAuth.getCurrentUser() != null) {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }else{
+                    Intent intent = new Intent(getApplicationContext(), IntroSliderActivity.class);
+                    startActivity(intent);
+                }
                 finish();
             }
         }, 4000);
