615c58ec3f431987098f6fef70d329157959f8ad
Shashank Verma <shashank.verma2002@gmail.com>
2023-01-08 00:57:47 +0530

    SplashScreenActivity: Remove unnecessary 4 second delay
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- app/src/main/java/com/example/notificationapp/view/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/example/notificationapp/view/activities/SplashScreenActivity.kt
@@ -2,13 +2,9 @@ package com.example.notificationapp.view.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.notificationapp.R
 import com.example.notificationapp.databinding.ActivitySplashScreenBinding
-import com.google.android.material.imageview.ShapeableImageView
 import com.google.firebase.auth.FirebaseAuth
 
 class SplashScreenActivity : AppCompatActivity() {
@@ -27,21 +23,16 @@ class SplashScreenActivity : AppCompatActivity() {
         binding.image.animate().translationY(-1600f).setDuration(1000).startDelay = 3000
         binding.text.animate().translationY(1400f).setDuration(1000).startDelay = 3000
 
-        val handler = Handler(mainLooper)
-        handler.postDelayed({
-            if (mAuth.currentUser != null) {
-                if (mAuth.currentUser!!.isEmailVerified) {
-                    val intent = Intent(applicationContext, MainActivity::class.java)
-                    startActivity(intent)
-                } else {
-                    mAuth.signOut()
-                    Toast.makeText(applicationContext, "Please Verify Your Email.", Toast.LENGTH_LONG).show()
-                }
+        if (mAuth.currentUser != null) {
+            if (mAuth.currentUser!!.isEmailVerified) {
+                startActivity(Intent(applicationContext, MainActivity::class.java))
             } else {
-                val intent = Intent(applicationContext, IntroSliderActivity::class.java)
-                startActivity(intent)
+                mAuth.signOut()
+                Toast.makeText(applicationContext, "Please Verify Your Email.", Toast.LENGTH_LONG).show()
             }
-            finish()
-        }, 4000)
+        } else {
+            startActivity(Intent(applicationContext, IntroSliderActivity::class.java))
+        }
+        finish()
     }
 }
