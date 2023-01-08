2a02d41a5da45609e05f1503dc045ec73bc2179f
Shashank Verma <shashank.verma2002@gmail.com>
2023-01-08 20:09:23 +0530

    ClubListAdapter: Fix OnClickListener
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- app/src/main/java/com/example/notificationapp/data/adapters/ClubListAdapter.kt
+++ app/src/main/java/com/example/notificationapp/data/adapters/ClubListAdapter.kt
@@ -11,6 +11,7 @@ import com.example.notificationapp.R
 import com.example.notificationapp.data.adapters.ClubListAdapter.CustomVH
 import com.example.notificationapp.data.network.ClubModel
 import com.example.notificationapp.view.activities.ClubActivity
+import com.google.android.material.card.MaterialCardView
 
 class ClubListAdapter(private val mClubs: List<ClubModel>, private val mContext: Context) : RecyclerView.Adapter<CustomVH>() {
 
@@ -18,32 +19,27 @@ class ClubListAdapter(private val mClubs: List<ClubModel>, private val mContext:
         CustomVH(View.inflate(parent.context, R.layout.club_list_item, null))
 
     override fun onBindViewHolder(holder: CustomVH, position: Int) {
-        holder.setView(mClubs[position])
+        holder.bindView(mClubs[position])
     }
 
     override fun getItemCount(): Int = mClubs.size
 
     inner class CustomVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        private val name: AppCompatTextView
-        private val description: AppCompatTextView
+        private val name: AppCompatTextView = itemView.findViewById(R.id.club_name)
+        private val description: AppCompatTextView = itemView.findViewById(R.id.textDescription)
+        private val background: MaterialCardView = itemView.findViewById(R.id.item_background)
 
-        init {
-            name = itemView.findViewById(R.id.club_name)
-            description = itemView.findViewById(R.id.textDescription)
-
-            itemView.setOnClickListener {
+        fun bindView(clubModel: ClubModel) {
+            name.text = clubModel.name
+            description.text = clubModel.description
+            background.setOnClickListener {
                 val intent = Intent(mContext, ClubActivity::class.java)
                 intent.putExtra(Constants.CLUB_NAME, mClubs[adapterPosition].name)
                 intent.putExtra(Constants.CLUB_ID, mClubs[adapterPosition].id)
                 intent.putExtra(Constants.CLUB_DESC, mClubs[adapterPosition].description)
                 mContext.startActivity(intent)
             }
-        }
-
-        fun setView(clubModel: ClubModel) {
-            name.text = clubModel.name
-            description.text = clubModel.description
         }
     }
 }
