65276061dce8d66d17dbb22544d754ce2d187bc3
Shashank Verma <shashank.verma2002@gmail.com>
2023-01-09 12:39:05 +0530

    Add FCM service
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

---
+++ app/src/main/java/com/example/notificationapp/app/AppFCMService.kt
@@ -0,0 +1,27 @@
+package com.example.notificationapp.app
+
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class AppFCMService : FirebaseMessagingService() {
+
+    companion object {
+        private const val TAG = "AppFCMService"
+    }
+
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "onNewToken")
+        super.onNewToken(token)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        Log.d(TAG, "onMessageReceived: ${message.from}")
+
+        val user = FirebaseAuth.getInstance().currentUser
+        user ?: return
+        Log.d(TAG, "onMessageReceived: ${message.data}")
+    }
+}
