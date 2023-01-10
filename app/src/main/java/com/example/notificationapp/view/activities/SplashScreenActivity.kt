c238f6eb9b2f724d5859f51f501bed24849cfcab
Shashank Verma <shashank.verma2002@gmail.com>
2023-01-10 20:29:23 +0530

    SplashScreenActivity: Add runtime permission for notification for Android 13
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- app/src/main/java/com/example/notificationapp/view/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/example/notificationapp/view/activities/SplashScreenActivity.kt
@@ -1,9 +1,15 @@
 package com.example.notificationapp.view.activities
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.notificationapp.app.UserInstance
 import com.example.notificationapp.databinding.ActivitySplashScreenBinding
@@ -14,6 +20,18 @@ class SplashScreenActivity : AppCompatActivity() {
     private lateinit var mAuth: FirebaseAuth
     private lateinit var binding: ActivitySplashScreenBinding
 
+    private val requestPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+        if (it) {
+            openApp()
+        } else {
+            Toast.makeText(
+                this, "Please enable notification permission for this app",
+                Toast.LENGTH_SHORT
+            ).show()
+            finish()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen().setKeepOnScreenCondition { true }
         super.onCreate(savedInstanceState)
@@ -22,11 +40,19 @@ class SplashScreenActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         mAuth = FirebaseAuth.getInstance()
-        val user = mAuth.currentUser
 
         binding.image.animate().translationY(-1600f).setDuration(1000).startDelay = 3000
         binding.text.animate().translationY(1400f).setDuration(1000).startDelay = 3000
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            validateNotificationPermission()
+        } else {
+            openApp()
+        }
+    }
+
+    private fun openApp() {
+        val user = mAuth.currentUser
         if (user != null) {
             if (user.isEmailVerified) {
                 UserInstance.refreshUserSession(user, applicationContext, {
@@ -47,6 +73,27 @@ class SplashScreenActivity : AppCompatActivity() {
         } else {
             startActivity(Intent(applicationContext, IntroSliderActivity::class.java))
             finish()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun validateNotificationPermission() {
+        when {
+            (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS))
+                    == PackageManager.PERMISSION_GRANTED -> {
+                openApp()
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS) -> {
+                Toast.makeText(
+                    this, "Please enable notification permission for this app",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+                return
+            }
+            else -> {
+                requestPermission.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
         }
     }
 }
