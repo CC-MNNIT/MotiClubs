5757cf343c16322683dc8bb849b22d010e4e3c73
Shashank Verma <shashank.verma2002@gmail.com>
2023-01-10 13:34:59 +0530

    ProfileFragment: Compress bitmap before uploading
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- app/src/main/java/com/example/notificationapp/view/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/notificationapp/view/fragments/ProfileFragment.kt
@@ -15,17 +15,15 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.fragment.app.Fragment
-import com.example.notificationapp.Constants
-import com.example.notificationapp.data.network.ProfilePicResponse
-import com.example.notificationapp.data.network.UserResponse
-import com.example.notificationapp.data.network.api.RetrofitAccessObject
+import com.example.notificationapp.app.UserInstance
+import com.example.notificationapp.data.network.API
 import com.example.notificationapp.databinding.FragmentProfileBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.squareup.picasso.NetworkPolicy
+import com.squareup.picasso.Picasso
+import java.io.ByteArrayOutputStream
 
 class ProfileFragment : Fragment() {
 
@@ -33,17 +31,13 @@ class ProfileFragment : Fragment() {
         private const val TAG = "ProfileFragment"
     }
 
-    private var user: UserResponse? = null
     private lateinit var binding: FragmentProfileBinding
-    private lateinit var mAuth: FirebaseAuth
     private var launcher =
         registerForActivityResult<Void?, Uri>(Contract(), ActivityResultCallback { result ->
             if (result == null) {
                 return@ActivityResultCallback
             }
             try {
-//                val selectedImage = compressAsyncBitmap(result)
-//                binding.profilePic.setImageBitmap(selectedImage)
                 Log.d(TAG, "calling updateUserProfile")
                 updateProfilePicture(result)
             } catch (ex: Exception) {
@@ -58,7 +52,7 @@ class ProfileFragment : Fragment() {
     ): View {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root = binding.root
-        getUserData()
+        setProfileValues()
         setListeners()
         return root
     }
@@ -81,22 +75,19 @@ class ProfileFragment : Fragment() {
     }
 
     private fun updateProfilePicture(imageUri: Uri) {
-        val preferences = requireContext().getSharedPreferences(
-            Constants.SHARED_PREFERENCE,
-            Context.MODE_PRIVATE
-        )
-
-        Log.d(TAG, "updateUserProfile function started")
-
-        var storage = Firebase.storage
-        val storageRef = storage.reference
+        val storageRef = Firebase.storage.reference
         val profilePicRef =
             storageRef.child("profile_images").child(FirebaseAuth.getInstance().currentUser!!.uid)
                 .child(FirebaseAuth.getInstance().currentUser!!.uid)
 
-        var uploadTask = profilePicRef.putFile(imageUri)
-
-        val urlTask = uploadTask.continueWithTask { task ->
+        val bitmap = compressBitmap(imageUri)
+        if (bitmap == null) {
+            Toast.makeText(requireContext(), "Internal bitmap error", Toast.LENGTH_SHORT).show()
+            return
+        }
+        val boas = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, boas)
+        profilePicRef.putBytes(boas.toByteArray()).continueWithTask { task ->
             if (!task.isSuccessful) {
                 task.exception?.let {
                     Log.d(TAG, "not success$it")
@@ -106,90 +97,45 @@ class ProfileFragment : Fragment() {
             profilePicRef.downloadUrl
         }.addOnCompleteListener { task ->
             if (task.isSuccessful) {
+                Log.d(TAG, "updateProfilePicture: got url")
                 val downloadUri = task.result
-                Log.d(TAG, downloadUri.toString())
-                Log.d(TAG, "started retrofit .........")
-
-                RetrofitAccessObject.getRetrofitAccessObject().updateProfilePic(
-                    preferences.getString(Constants.TOKEN, ""),
-                    ProfilePicResponse(downloadUri.toString())
-                )
-                    .enqueue(object : Callback<ProfilePicResponse?> {
-                        override fun onResponse(
-                            call: Call<ProfilePicResponse?>,
-                            response: Response<ProfilePicResponse?>
-                        ) {
-                            if (response.isSuccessful && response.body() != null) {
-                                user!!.avatar = response.body()!!.avatar
-                                Toast.makeText(context, "Saved on mongoDB", Toast.LENGTH_SHORT).show()
-                                Log.d(TAG, "saved on mongodb")
-                                setProfileValues()
-                            }else{
-                                Log.d(TAG, response.code().toString())
-                            }
-                        }
-                        override fun onFailure(call: Call<ProfilePicResponse?>, t: Throwable) {
-                            Log.d(TAG, "retrofit failed")
-                            Toast.makeText(context, "failed retrofit", Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                    )
+                API.updateProfilePic(UserInstance.getAuthToken(requireContext()), downloadUri.toString(), {
+                    Toast.makeText(requireContext(), "Profile pic updated", Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, "saved on mongodb")
+                    UserInstance.setAvatar(it.avatar)
+                    setProfileValues()
+                }) { Toast.makeText(requireContext(), "$it: Could not update profile pic", Toast.LENGTH_SHORT).show() }
             } else {
-                Toast.makeText(context, "Saved but still error", Toast.LENGTH_SHORT).show()
-                Log.d(TAG, "saved but still error")
+                Toast.makeText(requireContext(), "DB: Could not update profile pic", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-//    private fun encodeImage(bm: Bitmap?): String {
-//        val baos = ByteArrayOutputStream()
-//        bm?.compress(Bitmap.CompressFormat.JPEG, 100, baos)
-//        val b = baos.toByteArray()
-//        return Base64.encodeToString(b, Base64.DEFAULT)
-//    }
-
-//    private fun decodeImage(imageString: String?): Bitmap {
-//        val imageBytes = Base64.decode(imageString, Base64.DEFAULT)
-//        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
-//    }
-
-    private fun getUserData() {
-        val preferences =
-            requireContext().getSharedPreferences(Constants.SHARED_PREFERENCE, Context.MODE_PRIVATE)
-        RetrofitAccessObject.getRetrofitAccessObject()
-            .getUserData(preferences.getString(Constants.TOKEN, ""))
-            .enqueue(object : Callback<UserResponse?> {
-                override fun onResponse(
-                    call: Call<UserResponse?>,
-                    response: Response<UserResponse?>
-                ) {
-                    if (response.isSuccessful && response.body() != null) {
-                        user = response.body()!!
-                        setProfileValues()
-                    }
-                }
-
-                override fun onFailure(call: Call<UserResponse?>, t: Throwable) {}
-            })
-    }
-
     private fun setProfileValues() {
-        if (user == null) return
-        binding.emailTv.text = user!!.email
-        binding.tvName.text = user!!.name
-        binding.nameTv1.text = user!!.name
-        binding.mobileTv.text = user!!.phoneNumber
-        binding.tvRegNo.text = user!!.registrationNumber
-        binding.yearTv.text = user!!.graduationYear
-        binding.courseTv.text = user!!.course
+        binding.emailTv.text = UserInstance.getEmail()
+        Log.d(TAG, "setProfileValues: ${UserInstance.getName()}")
+        Log.d(TAG, "setProfileValues: ${UserInstance.getEmail()}")
+        Log.d(TAG, "setProfileValues: ${UserInstance.getAvatar()}")
+        binding.tvName.text = UserInstance.getName()
+        binding.nameTv1.text = UserInstance.getName()
+        binding.mobileTv.text = UserInstance.getPhoneNumber()
+        binding.tvRegNo.text = UserInstance.getRegNo()
+        binding.yearTv.text = UserInstance.getGradYear()
+        binding.courseTv.text = UserInstance.getCourse()
 
-//        if (user!!.avatar?.isNotEmpty() == true) {
-//            //
-//        }
+        val avatar = UserInstance.getAvatar()
+        if (avatar.isEmpty()) return
+
+        Picasso.get().load(avatar).networkPolicy(NetworkPolicy.OFFLINE).into(binding.profilePic, object : com.squareup.picasso.Callback {
+            override fun onSuccess() {}
+
+            override fun onError(e: java.lang.Exception?) {
+                Picasso.get().load(avatar).into(binding.profilePic)
+            }
+        })
     }
 
-    //    fun compressBitmap(uri : Uri): Bitmap? = CoroutineScope.launch { compressAsyncBitmap(uri) }
-    private fun compressAsyncBitmap(uri: Uri): Bitmap? {
+    private fun compressBitmap(uri: Uri): Bitmap? {
         val options = BitmapFactory.Options()
         options.inJustDecodeBounds = true
         val ins = requireContext().contentResolver.openInputStream(uri)
@@ -197,7 +143,7 @@ class ProfileFragment : Fragment() {
         ins?.close()
 
         var scale = 1
-        while (options.outWidth / scale / 2 >= 100 && options.outHeight / scale / 2 >= 100) {
+        while (options.outWidth / scale / 2 >= 200 && options.outHeight / scale / 2 >= 200) {
             scale *= 2
         }
 
