a2f7646a78eed464d7a6d643ab81c18922f17c0a
Shashank Verma <shashank.verma2002@gmail.com>
2023-01-10 20:28:48 +0530

    AppFCM: Implement notification
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- app/src/main/java/com/example/notificationapp/app/AppFCMService.kt
+++ app/src/main/java/com/example/notificationapp/app/AppFCMService.kt
@@ -1,10 +1,24 @@
 package com.example.notificationapp.app
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.os.Build
+import android.os.Handler
 import android.util.Log
-import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import com.example.notificationapp.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.Target
 
 class AppFCMService : FirebaseMessagingService() {
 
@@ -22,6 +36,91 @@ class AppFCMService : FirebaseMessagingService() {
 
         val user = FirebaseAuth.getInstance().currentUser
         user ?: return
-        Log.d(TAG, "onMessageReceived: ${message.data}")
+        Log.d(TAG, "onMessageReceived")
+        Handler(mainLooper).post { postNotificationLegacy(message.data) }
+    }
+
+    private fun postNotificationLegacy(data: Map<String, String>) {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val clubName = data["clubName"] ?: ""
+        val clubID = data["club"] ?: ""
+        val message = data["message"] ?: ""
+        val adminName = data["adminName"] ?: ""
+        val url = data["adminAvatar"] ?: ""
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            postNotificationCompat(notificationManager, clubID, clubName, adminName, message, url)
+        } else {
+            postNotificationLegacy(adminName, clubName, message, url, notificationManager)
+        }
+    }
+
+    private fun postNotificationLegacy(
+        adminName: String,
+        clubName: String,
+        message: String,
+        url: String,
+        notificationManager: NotificationManager
+    ) {
+        val notificationHandler = Notification.Builder(applicationContext)
+            .setContentTitle("$adminName posted in $clubName")
+            .setContentText(message)
+            .setColor(ContextCompat.getColor(applicationContext, R.color.main_color))
+            .setSmallIcon(R.drawable.notification)
+            .setStyle(Notification.BigTextStyle())
+            .setPriority(Notification.PRIORITY_HIGH)
+
+        Picasso.get().load(url).into(object : Target {
+            override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+                notificationHandler.setLargeIcon(bitmap)
+                Log.d(TAG, "postNotification: loaded profile icon")
+            }
+
+            override fun onBitmapFailed(e: Exception?, errorDrawable: Drawable?) {
+            }
+
+            override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
+            }
+        })
+        notificationManager.notify(0, notificationHandler.build())
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun postNotificationCompat(
+        notificationManager: NotificationManager,
+        clubID: String,
+        clubName: String,
+        adminName: String,
+        message: String,
+        url: String
+    ) {
+        notificationManager.createNotificationChannel(NotificationChannel(clubID, clubName, NotificationManager.IMPORTANCE_HIGH).apply {
+            lightColor = Color.BLUE
+            enableVibration(true)
+            enableLights(true)
+        })
+
+        val notificationHandler = NotificationCompat.Builder(applicationContext, clubID)
+            .setContentTitle("$adminName posted in $clubName")
+            .setContentText(message)
+            .setColorized(true)
+            .setColor(ContextCompat.getColor(applicationContext, R.color.main_color))
+            .setSmallIcon(R.drawable.notification)
+            .setStyle(NotificationCompat.BigTextStyle())
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+        Picasso.get().load(url).into(object : Target {
+            override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+                notificationHandler.setLargeIcon(bitmap)
+                Log.d(TAG, "postNotification: loaded profile icon")
+            }
+
+            override fun onBitmapFailed(e: Exception?, errorDrawable: Drawable?) {
+            }
+
+            override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
+            }
+        })
+        notificationManager.notify(0, notificationHandler.build())
     }
 }
