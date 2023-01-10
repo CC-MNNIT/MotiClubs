43c8deb1f024b1dbe6c786bf3e567d557c72d8e5
Shashank Verma <shashank.verma2002@gmail.com>
2023-01-10 13:32:39 +0530

    PostListAdapter: Fix loading of profile pic
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- app/src/main/java/com/example/notificationapp/data/adapters/PostListAdapter.kt
+++ app/src/main/java/com/example/notificationapp/data/adapters/PostListAdapter.kt
@@ -1,24 +1,19 @@
 package com.example.notificationapp.data.adapters
 
 import android.content.Context
-import android.content.Intent
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.notificationapp.Constants
 import com.example.notificationapp.R
-import com.example.notificationapp.data.network.AdminResponse
+import com.example.notificationapp.app.UserInstance
+import com.example.notificationapp.data.network.API
 import com.example.notificationapp.data.network.PostResponse
-import com.example.notificationapp.data.network.api.RetrofitAccessObject
-import com.example.notificationapp.view.activities.ClubActivity
 import com.google.android.material.card.MaterialCardView
+import com.squareup.picasso.NetworkPolicy
 import com.squareup.picasso.Picasso
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -43,7 +38,7 @@ class PostListAdapter(private val mPosts: List<PostResponse>, private val mConte
         private val name: AppCompatTextView = itemView.findViewById(R.id.admin_name)
         private val description: AppCompatTextView = itemView.findViewById(R.id.textDescription)
         private val background: MaterialCardView = itemView.findViewById(R.id.item_background)
-        private val profilePic: ImageView? = itemView.findViewById(R.id.profile_pic)
+        private val profilePic: ImageView = itemView.findViewById(R.id.admin_profile_pic)
         private val dateTime: AppCompatTextView = itemView.findViewById(R.id.post_time)
 
         fun bindView(postResponse: PostResponse) {
@@ -51,33 +46,24 @@ class PostListAdapter(private val mPosts: List<PostResponse>, private val mConte
             description.text = postResponse.message
             dateTime.text = convertLongToTime(postResponse.time)
 
-            var adminResponse: AdminResponse? = null
-            val preferences = mContext.getSharedPreferences(Constants.SHARED_PREFERENCE, Context.MODE_PRIVATE)
+            API.getUserDetails(UserInstance.getAuthToken(mContext), postResponse.adminEmail, {
+                name.text = it.name
+                if (it.avatar.isEmpty()) return@getUserDetails
+                Picasso.get().load(it.avatar).networkPolicy(NetworkPolicy.OFFLINE).into(profilePic, object : com.squareup.picasso.Callback {
+                    override fun onSuccess() {}
 
-            RetrofitAccessObject.getRetrofitAccessObject()
-                .getAdminDetails(preferences.getString(Constants.TOKEN, ""), postResponse.adminEmail)
-                .enqueue(object : Callback<AdminResponse?> {
-                    override fun onResponse(
-                        call: Call<AdminResponse?>,
-                        response: Response<AdminResponse?>
-                    ) {
-                        adminResponse = response.body()!!
-                        name.text = adminResponse!!.name
-//                        if (adminResponse!!.avatar != null) {
-//                            Picasso.get().load(adminResponse!!.avatar).placeholder(R.drawable.ic_person).into(profilePic)
-//                        }
-                    }
-
-                    override fun onFailure(call: Call<AdminResponse?>, t: Throwable) {
+                    override fun onError(e: Exception?) {
+                        Picasso.get().load(it.avatar).into(profilePic)
                     }
                 })
-            background.setOnClickListener {
-                val intent = Intent(mContext, ClubActivity::class.java)
-//                intent.putExtra(Constants.CLUB_NAME, mPosts[adapterPosition].name)
-//                intent.putExtra(Constants.CLUB_ID, mPosts[adapterPosition].id)
-//                intent.putExtra(Constants.CLUB_DESC, mClubs[adapterPosition].description)
-                mContext.startActivity(intent)
-            }
+            }) {}
+//            background.setOnClickListener {
+//                val intent = Intent(mContext, ClubActivity::class.java)
+////                intent.putExtra(Constants.CLUB_NAME, mPosts[adapterPosition].name)
+////                intent.putExtra(Constants.CLUB_ID, mPosts[adapterPosition].id)
+////                intent.putExtra(Constants.CLUB_DESC, mClubs[adapterPosition].description)
+//                mContext.startActivity(intent)
+//            }
         }
     }
 
