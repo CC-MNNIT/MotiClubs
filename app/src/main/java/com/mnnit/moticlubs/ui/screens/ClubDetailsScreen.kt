b1701948bd80a53f867937ee4f61f025e5bcf920
hackeramitkumar <amit9116260192@gmail.com>
2023-02-01 23:18:22 +0530

    added social media dialog

--- app/src/main/java/com/mnnit/moticlubs/ui/screens/ClubDetailsScreen.kt
+++ app/src/main/java/com/mnnit/moticlubs/ui/screens/ClubDetailsScreen.kt
@@ -3,18 +3,18 @@ package com.mnnit.moticlubs.ui.screens
 import android.util.Patterns
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.AddAPhoto
+import androidx.compose.material.icons.rounded.Chat
+import androidx.compose.material.icons.rounded.Facebook
 import androidx.compose.material3.*
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.runtime.Composable
@@ -22,13 +22,18 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModel
 import coil.compose.rememberAsyncImagePainter
@@ -52,7 +57,8 @@ class ClubDetailsScreenViewModel @Inject constructor() : ViewModel() {
     val initialClubModel = mutableStateOf(ClubModel("", "", "", "", listOf(), listOf()))
     val description = mutableStateOf("")
     val avatar_url = mutableStateOf("")
-
+    val showLinkDialog = mutableStateOf(false)
+    lateinit var socialMedia : List<String>
     val isLoading = mutableStateOf(false)
 
     val isEditButtonEnabled
@@ -66,6 +72,7 @@ fun ClubDetailsScreen(appViewModel: AppViewModel, viewModel: ClubDetailsScreenVi
     viewModel.initialClubModel.value = appViewModel.clubModel.value
     viewModel.description.value = appViewModel.clubModel.value.description
     viewModel.avatar_url.value = appViewModel.clubModel.value.avatar
+    viewModel.socialMedia = appViewModel.clubModel.value.socialMedia
 
     MotiClubsTheme(colorScheme = getColorScheme()) {
         SetNavBarsTheme()
@@ -170,7 +177,43 @@ fun ClubProfilePic(viewModel: ClubDetailsScreenViewModel, modifier: Modifier = M
 fun ClubInfo(viewModel: ClubDetailsScreenViewModel, modifier: Modifier = Modifier) {
     val scrollState = rememberScrollState()
     Column(modifier = modifier.verticalScroll(scrollState)) {
-        Text(viewModel.initialClubModel.value.name, fontSize = 24.sp, fontWeight = FontWeight.SemiBold)
+        if (viewModel.showLinkDialog.value) {
+            InputSocialLinkDialog(viewModel = viewModel)
+        }
+     Row() {
+         Text(viewModel.initialClubModel.value.name, fontSize = 24.sp, fontWeight = FontWeight.SemiBold)
+         Row(modifier = Modifier.padding(start = 45.dp)) {
+         IconButton(
+             onClick = {
+                    /* TODO */
+             },
+             modifier = Modifier
+                 .align(Alignment.Bottom)
+                 .border(0.dp, getColorScheme().onSurface, shape = RoundedCornerShape(24.dp))
+         ) {
+             Icon(painter = rememberVectorPainter(image = Icons.Rounded.Facebook), contentDescription = "", tint = Color.Blue)
+         }
+         IconButton(
+             onClick = {
+                 /* TODO */
+             },
+             modifier = Modifier
+                 .align(Alignment.Bottom)
+                 .border(0.dp, getColorScheme().onSurface, shape = RoundedCornerShape(24.dp))
+         ) {
+             Icon(painter = rememberVectorPainter(image = Icons.Rounded.Chat), contentDescription = "", tint = Color.Red)
+         }
+             IconButton(
+                 onClick = { viewModel.showLinkDialog.value = true },
+                 modifier = Modifier
+                     .align(Alignment.Bottom)
+                     .border(0.dp, getColorScheme().onSurface, shape = RoundedCornerShape(24.dp))
+             ) {
+                 Icon(painter = rememberVectorPainter(image = Icons.Rounded.Add), contentDescription = "")
+             }
+         }
+     }
+
         OutlinedTextField(
             modifier = Modifier.fillMaxWidth(),
             value = viewModel.description.value,
@@ -184,5 +227,71 @@ fun ClubInfo(viewModel: ClubDetailsScreenViewModel, modifier: Modifier = Modifie
                 disabledLeadingIconColor = contentColorFor(backgroundColor = colorScheme.background)
             )
         )
+
     }
 }
+
+
+@Composable
+fun InputSocialLinkDialog(viewModel: ClubDetailsScreenViewModel) {
+    val colorScheme = getColorScheme()
+    Dialog(onDismissRequest = { viewModel.showLinkDialog.value = false }, DialogProperties()) {
+        Box(
+            modifier = Modifier
+                .padding(16.dp)
+                .clip(RoundedCornerShape(24.dp))
+                .background(colorScheme.background)
+        ) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    "Facebook URL",
+                    fontSize = 16.sp,
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .align(Alignment.CenterHorizontally),
+                    fontWeight = FontWeight.SemiBold
+                )
+
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = viewModel.socialMedia[0],
+                    onValueChange = { viewModel.socialMedia = listOf(it,viewModel.socialMedia.get(1)) },
+                    shape = RoundedCornerShape(24.dp),
+                    label = { Text(text = "Instagram URL") },
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Text),
+                )
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = viewModel.socialMedia[1],
+                    onValueChange = { viewModel.socialMedia = listOf(viewModel.socialMedia.get(0),it) },
+                    shape = RoundedCornerShape(24.dp),
+                    label = { Text(text = "Link") },
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Text),
+                )
+
+                TextButton(
+                    onClick = {
+                        viewModel.showLinkDialog.value = false
+                    },
+                    modifier = Modifier
+                        .padding(top = 8.dp)
+                        .align(Alignment.CenterHorizontally),
+                    enabled = ( (viewModel.socialMedia.get(0).matches(Patterns.WEB_URL.toRegex())) || (viewModel.socialMedia.get(0).matches(Patterns.WEB_URL.toRegex()))) && ( (viewModel.socialMedia.get(1).isEmpty()) || (viewModel.socialMedia.get(1).isEmpty()))
+                ) {
+                    Text(
+                        text = "Add links",
+                        color = if (( (viewModel.socialMedia.get(0).matches(Patterns.WEB_URL.toRegex())) || (viewModel.socialMedia.get(0).matches(Patterns.WEB_URL.toRegex()))) && ( (viewModel.socialMedia.get(1).isEmpty()) || (viewModel.socialMedia.get(1).isEmpty()))) {
+                            colorScheme.primary
+                        } else colorScheme.contentColorFor(colorScheme.background),
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                }
+            }
+        }
+    }
+
+}
+
