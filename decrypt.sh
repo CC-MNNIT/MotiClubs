65a9ec77cccd1343ebd9206ee2ebe6314a45f03a
Shashank Verma <shashank.verma2002@gmail.com>
2023-02-07 00:13:57 +0530

    decrypt.sh: Update variable
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- decrypt.sh
+++ decrypt.sh
@@ -2,5 +2,5 @@
 
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$JSON_PASS" \
+gpg --quiet --batch --yes --decrypt --passphrase="$JSON" \
 --output app/google-services.json app/google-services.json.gpg
